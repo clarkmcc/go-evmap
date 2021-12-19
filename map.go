b37927bb3766f671476a7706b5106d49493ba25b
Clark McCauley <clark.mccauley@printtrackerpro.com>
2021-12-19 15:52:57 -0700

    Removed accidental copyrights

--- map.go
+++ map.go
@@ -1,12 +1,3 @@
-/*
-Copyright (C) 2020 Print Tracker, LLC - All Rights Reserved
-
-Unauthorized copying of this file, via any medium is strictly prohibited
-as this source code is proprietary and confidential. Dissemination of this
-information or reproduction of this material is strictly forbidden unless
-prior written permission is obtained from Print Tracker, LLC.
-*/
-
 package eventual
 
 import (
